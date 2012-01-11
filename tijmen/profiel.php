2d28a6a7d4c73309dd7220d2ca6618df894ffbc6
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2012-01-11 18:16:25 +0100

    toevoeging van profielpagina met css. nog niet af, voorlopige versie

---
+++ tijmen/profiel.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<title>Projectweek HTML 1</title>
+</head>
+
+<body>
+<div class="container">
+    <div class="banner">
+    	<img src="logo.png" align="middle" />
+    </div>
+    <div class="menu">
+    	<div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='index.php'">
+    		<b>Forum</b>
+    	</div>
+        <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='profiel.php'">
+    		<b>Profiel</b>
+    	</div>
+        <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
+    		<b>Registratie</b>
+    	</div>
+        <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
+    		<b>Log in</b>
+    	</div>
+    </div>
+  <div class="navigation">
+   	U bent hier: <b>Profiel</b>
+  </div>
+  <div class="content"><h1>insert Username</h1>
+	<div class="profileinfo">
+		<div class="catbalk">User Information</div>
+		<div class="forumhok">Hallo daar</div>
+	</div>
+	<div class="profilecomments">
+		<div class="catbalk">Latest Comments</div>
+		<div class="forumhok">
+			Comment1<br>
+			Comment2
+			<!-- hierbinnen moet met een scriptje de laatste 10 comments van de user
+				 worden opgezocht. -->
+		</div>
+	</div>
+	<div class="profilefooter">
+	<!-- dit zorgt ervoor dat de body genoeg naar beneden wordt gestrecht ondanks dat
+		 content float.-->
+	</div>
+	
+  </div>
+    
+</div>
+<div class="footer">
+	&copy; 2012 - ShoreDog Power Services, Inc.&trade; (Powered by ShoreDog Forum Engine).
+</div>
+</body>
+</html>
