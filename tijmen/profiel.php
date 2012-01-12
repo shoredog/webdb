00534b2e9a8b60a8cde6647158e8e935deef5b92
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2012-01-12 11:35:06 +0100

    nieuwe wijziging

--- tijmen/profiel.php
+++ tijmen/profiel.php
@@ -32,16 +32,41 @@ onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href
   <div class="navigation">
    	U bent hier: <b>Profiel</b>
   </div>
-  <div class="content"><h1>insert Username</h1>
+  <div class="content">
+	<div class="userinfoheader">
+		<img src="avatar.jpg" width="50px" height="50px" class="avatar" />
+		<h1>Tijmen Zwaan</h1>
+	</div>
 	<div class="profileinfo">
 		<div class="catbalk">User Information</div>
-		<div class="forumhok">Hallo daar</div>
+		<div class="forumhok">
+			<table class="userinfotable">
+				<tr>
+					<td>Name:</td>
+					<td>Tijmen</td>
+				</tr>
+				<tr>
+					<td>Gender:</td>
+					<td>Male</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 	<div class="profilecomments">
 		<div class="catbalk">Latest Comments</div>
 		<div class="forumhok">
-			Comment1<br>
-			Comment2
+			<div class="comment">
+				<div class="catbalk">Comment 1</div>
+				<div class="forumhok">hier staat dan de inhoud van comment 1</div>
+			</div>
+			<div class="comment">
+				<div class="catbalk">Comment 2</div>
+				<div class="forumhok">hier staat dan de inhoud van comment 2</div>
+			</div>
+			<div class="comment">
+				<div class="catbalk">Comment 3</div>
+				<div class="forumhok">hier staat dan de inhoud van comment 3</div>
+			</div>
 			<!-- hierbinnen moet met een scriptje de laatste 10 comments van de user
 				 worden opgezocht. -->
 		</div>
