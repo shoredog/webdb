1ccf10a1da3d6c6cd2cc1dd28a2e29ce91f1eb82
David van Erkelens <dve@live.nl>
2012-01-12 16:33:41 +0100

    Update einde donderdag

--- David/adminindex.php
+++ David/adminindex.php
@@ -31,54 +31,53 @@
             </div>
         </div>
         <div class="navigation">
-            U bent hier: <b>Gebruikerspaneel</b>
+            U bent hier: <b>Administratiepaneel</b>
         </div>
         
-        <div class="content"><div class="paneelmenu">
-            <div class="catbalk">
-                Menu
-            </div>
-          	<div class="paneelbox">
-                - Home<br />
-                - Foruminstellingen<br />
-                - Avatar wijzigen<br />
-                - Signature wijzigen<br />
-                - Wachtwoord wijzigen
-           	</div>
+        <div class="content">
+        <center><h1>Administratiepaneel</h1></center>
+        	<div class="paneelmenu">
+                <div class="catbalk">
+                    Menu
+                </div>
+                <div class="paneelbox">
+                    - Home<br />
+                    - Gebruiker bewerken<br />
+                    - Topic moderation<br />
+                    - Categoriemanagement<br />
+                    - Forummanagement<br />
+                    - Algemene instellingen
+                </div>
         </div>
         <div class="paneelcontent">
             <div class="catbalk">
-               	Wachtwoord wijzigen
+                Administratiepaneel
+            </div>
+            <div class="paneelbox">
+                Welkom in het administratiepaneel! Hier kan u verschillende aspecten van het forum naar wens aanpassen.
+            </div>
+            <div class="catbalk">
+                Adminkladblok
             </div>
           	<div class="paneelbox">
-      			<table width="100%"  class="invoertabel">
-                	<tr>
-                    	<td width="50%"><b>Uw oude wachtwoord</b></td>
-                        <td>
-                        	<input name="ondertitel" type="password" maxlength="250" class="paneelinvoer" />
-                    	</td>
-                    </tr>
-                    <tr>
-                    	<td width="50%"><b>Uw nieuwe wachtwoord</b></td>
-                        <td>
-                        	<input name="site" type="password" maxlength="250" class="paneelinvoer" />
-                    	</td>
-                    </tr>
-                    <tr>
-                    	<td width="50%"><b>Nogmaals uw nieuwe wachtwoord</b></td>
-                        <td>
-                        	<input name="ondertitel" type="password" maxlength="250" class="paneelinvoer" />
-                    	</td>
-                    </tr>
-           		</table>
-			</div>
+      			<form action="#" method="post" name="kladblok">
+                	<textarea name="kladbl" cols="10" rows="5" class="paneeltext">Hier kunt u informatie opslaan welke zichtbaar is voor u en andere admins.</textarea>
+                    <div align="right">
+                    	<input name="submitkladblok" type="submit" value="Opslaan" />
+                    </div>
+                </form>
+           	</div>
       	</div>
       	<div class="paneelfooter"></div>
    		</div>
 	</div>
 </div>
 <div class="footer">
-	&copy; 2012 - ShoreDog Power Services, Inc.&trade; (Powered by ShoreDog Forum Engine&reg;).
+	<center>
+    	<div class="footerdivider1" style="text-align:left">Gebruikersovereenkomst</div>
+        <div class="footerdivider2" style="text-align:center">&copy; 2012 - ShoreDog Power Services, Inc.&trade; (Powered by ShoreDog Forum Engine&reg;).</div>
+        <div class="footerdivider1" style="text-align:right">Contact</div>
+    </center>
 </div>
 </body>
 </html>
