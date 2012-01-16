546c047b1e026dc49ef655efa602166ee57bb4f4
Julien vanderWerve <jvdwerve@hotmail.com>
2012-01-16 15:33:30 +0100

    error

--- html_complete/index.php
+++ html_complete/index.php
@@ -3,74 +3,180 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="style1.0.css" rel="stylesheet" type="text/css" />
-<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico" />
 <title>Projectweek HTML 1</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="banner">
-            <center><img src="logo.png" align="middle" /></center>
-        </div>
-        <div class="menu">
-            <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
-    onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
-                <b>Forum</b>
-            </div>
-            <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
-    onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
-                <b>Gebruikers</b>
-            </div>
-            <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
-    onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
-                <b>Registratie</b>
-            </div>
-            <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
-    onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
-                <b>Log in</b>
-            </div>
-        </div>
-        <div class="navigation">
-            U bent hier: <b>Gebruikerspaneel</b>
-        </div>
-        
-        <div class="content"><div class="paneelmenu">
-            <div class="catbalk">
-                Menu
-            </div>
-          	<div class="paneelbox">
-                - Home<br />
-                - Foruminstellingen<br />
-                - Avatar wijzigen<br />
-                - Signature wijzigen<br />
-                - Wachtwoord wijzigen
-           	</div>
-        </div>
-        <div class="paneelcontent">
-            <div class="catbalk">
-                Gebruikerspaneel
-            </div>
-            <div class="paneelbox">
-                Welkom in het gebruikerspaneel! Hier kan u uw profiel naar behoefte aanpassen.
-            </div>
-            <div class="catbalk">
-                Persoonlijk kladblok
-            </div>
-          	<div class="paneelbox">
-      			<form action="#" method="post" name="kladblok">
-                	<textarea name="kladbl" cols="10" rows="5" class="paneeltext">Hier kunt u informatie opslaan welke u later nog kunt gebruiken.</textarea>
-                    <div align="right">
-                    	<input name="submitkladblok" type="submit" value="Opslaan" />
-                    </div>
-                </form>
-           	</div>
-      	</div>
-      	<div class="paneelfooter"></div>
-   		</div>
+<div class="container">
+    <div class="banner">
+    	<img src="logo.png" align="middle" />
+    </div>
+    <div class="menu">
+    	<div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='/categorie.php'">
+    		<b>Forum</b>
+    	</div>
+        <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
+    		<b>Gebruikers</b>
+    	</div>
+        <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
+    		<b>Registratie</b>
+    	</div>
+        <div class="menuitem" onMouseOver="this.style.backgroundColor='#000000';" 
+onMouseOut="this.style.backgroundColor='#333333';" onClick="window.location.href='yourlinklocationhere'">
+    		<b>Log in</b>
+    	</div>
+    </div>
+    <div class="navigation">
+    	U bent hier: <b>Forum 1</b>
+    </div>
+<div class="categoriecontent">
+	<div class="categoriecontainer">
+		<div class="categoriecatbalk">Forum 1</div>
+		<div class="forforumhok">
+			<div class="categorieinfocontainer">
+				<div class="categorieinfoforumhok" >
+					<b>Forum</b>
+				</div>
+				<div class="foruminfoposthok">
+					<center>
+						<b>Berichten</b>
+					</center>
+				</div>
+				<div class="categorieinfolastpost">
+					<b>Laatste bericht</b>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 1</b><br />
+					<i>Poster Topic 1</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 1</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 2</b><br />
+					<i>Poster Topic 2</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b></br>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 2</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 3</b><br />
+					<i>Poster Topic 3</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b></br>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 3</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 4</b><br />
+					<i>Poster Topic 4</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b></br>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 4</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 5</b><br />
+					<i>Poster Topic 5</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b></br>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 5</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 6</b><br />
+					<i>Poster Topic 6</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b></br>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 6</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class="categorieforumcontainer">
+				<div class="categorieforumhok" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Topic 7</b><br />
+					<i>Poster Topic 7</i>
+				</div>
+				<div class="forumposthok">
+					<center>
+						<b>100</b></br>
+					</center>
+				</div>
+				<div class="categorielastpost" onMouseOver="this.style.backgroundColor='#888888';"
+onMouseOut="this.style.backgroundColor='#666666';" onClick="window.location.href='yourlinklocationhere'">
+					<b>Laatste bericht 7</b></br>
+					<i>Op datum door poster</i>
+				</div>
+			</div>
+			<div class ="eindfloat"></div>
+		</div>
+		<div class="categoriefooter"></div>
 	</div>
 </div>
+    
+
 <div class="footer">
-	&copy; 2012 - ShoreDog Power Services, Inc.&trade; (Powered by ShoreDog Forum Engine&reg;).
+	&copy; 2012 - ShoreDog Power Services, Inc.&trade; (Powered by ShoreDog Forum Engine).
 </div>
 </body>
 </html>
