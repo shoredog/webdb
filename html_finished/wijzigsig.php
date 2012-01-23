7b39c5578fa937a37a049fdf0859091657d6a57f
Victor den Haan <vdenhaan@gmail.com>
2012-01-23 13:15:17 +0100

    Indentation

--- html_finished/wijzigsig.php
+++ html_finished/wijzigsig.php
@@ -5,35 +5,36 @@ include '/include/header.php';
             U bent hier: <b>Gebruikerspaneel</b>
         </div>
         
-        <div class="content"><div class="paneelmenu">
-            <div class="catbalk">
-                Menu
-            </div>
-          	<div class="paneelbox">
-                - Home<br />
-                - Foruminstellingen<br />
-                - Avatar wijzigen<br />
-                - Signature wijzigen<br />
-                - Wachtwoord wijzigen
-           	</div>
-        </div>
-        <div class="paneelcontent">
-            <div class="catbalk">
-               	Uw huidige signature
-            </div>
-          	<div class="paneelbox">
-      			<center>
-                	<img src="http://1.bp.blogspot.com/_bBlNFyLU7Ik/TSzrlWs8kNI/AAAAAAAABgc/Gm91ApIeJPA/s1600/Woodrow_wilson_signature.png" width="300" />
-                </center>
+        <div class="content">
+			<div class="paneelmenu">
+				<div class="catbalk">
+					Menu
+				</div>
+				<div class="paneelbox">
+					- Home<br />
+					- Foruminstellingen<br />
+					- Avatar wijzigen<br />
+					- Signature wijzigen<br />
+					- Wachtwoord wijzigen
+				</div>
 			</div>
-            <div class="catbalk">
-               	 Uw signature bewerken
-            </div>
-          	<div class="paneelbox">
-      			<textarea name="signature" cols="10" rows="8" class="paneeltext">Haha tijmen is gay</textarea>
+			<div class="paneelcontent">
+				<div class="catbalk">
+					Uw huidige signature
+				</div>
+				<div class="paneelbox">
+					<center>
+						<img src="http://1.bp.blogspot.com/_bBlNFyLU7Ik/TSzrlWs8kNI/AAAAAAAABgc/Gm91ApIeJPA/s1600/Woodrow_wilson_signature.png" width="300" />
+					</center>
+				</div>
+				<div class="catbalk">
+					 Uw signature bewerken
+				</div>
+				<div class="paneelbox">
+					<textarea name="signature" cols="10" rows="8" class="paneeltext">Haha tijmen is gay</textarea>
+				</div>
 			</div>
-      	</div>
-      	<div class="paneelfooter"></div>
+			<div class="paneelfooter"></div>
    		</div>
 	</div>
 <?php
