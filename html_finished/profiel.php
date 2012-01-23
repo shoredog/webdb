60bb15918e297f9ea59898b33fab87ede378a819
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2012-01-23 22:56:42 +0100

    voorgang php toevoeging aan profielpagina

--- html_finished/profiel.php
+++ html_finished/profiel.php
@@ -1,28 +1,47 @@
 <?php
-include '/include/header.php';
+    include '/include/header.php';
 ?>
-
+<?php
+    $user = $_GET["user"];
+    $dinges = 'hallo';
+?>
 <div class="navigation">
 	U bent hier: <b>Profiel</b>
 </div>
 
 <div class="content">
 	<div class="userinfoheader">
-		<img src="avatar.jpg" class="avatar" />
-		<h1>Tijmen Zwaan</h1>
+        <?php
+            $result = mysql_query("SELECT * FROM users WHERE user_id=$user");
+            $user = mysql_fetch_array($result);
+            print "<img src=\"" . $user['avatar'] . "\" class=\"avatar\">";
+            print "<h1>" . $user['user_name'] . "</h1>";
+            print "<h2>" . $user['signature'] . "</h2>";
+        ?>
 	</div>
 	<div class="profileinfo">
 		<div class="catbalk">User Information</div>
 		<div class="forumhok">
 			<table class="userinfotable">
-				<tr>
-					<td class="a">Name:</td>
-					<td class="a">Tijmen</td>
-				</tr>
-				<tr>
-					<td class="a">Gender:</td>
-					<td class="a">Male</td>
-				</tr>
+                <?php
+                    print "<tr><td>Name:</td><td>";
+                        print $user['user_name'];
+                    print "</td></tr>";
+                    print "<tr><td>Rank:</td><td>";
+                        if ($user['rank']==2)
+                            print 'admin';
+                        else
+                            print 'user';
+                    print "</td></tr>";
+                    print "<tr><td>Location:</td><td>";
+                        print $user['location'];
+                    print "</td></tr>";
+                    if ($user['show_dob']==1){
+                        print "<tr><td>Birthdate:</td><td>";
+                            print $user['date_of_birth'];
+                        print "</td></tr>";
+                    }
+                ?>
 			</table>
 		</div>
 	</div>
